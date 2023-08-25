186c39358aa68a80b56e0066c2678d99659a9d4d
TashtemorovK <tashtemirovkadyrbek@gmail.com>
2023-08-25 18:10:54 +0600

    Initial commit

---
+++ app/src/main/java/com/example/thirdmonthless_5/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.thirdmonthless_5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().add(R.id.container, FirstFragment()).commit()
+    }
+}
